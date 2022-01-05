2b2dc96331beb176f23678d4786d825f66c655b2
Michael Hutchings <mdhutchings94@gmail.com>
2022-01-05 19:43:21 +0000

    update list of portfolio thing

--- src/routes/portfolio.svelte
+++ src/routes/portfolio.svelte
@@ -1,10 +1,36 @@
 <script>
-	export let stuff = 3;
+	// export let stuff = 3;
 </script>
 
 <span>Stuff I've made</span>
 
+<h2>Internal (Public) Projects from Work</h2>
 <ul>
+	<li>Monumental</li>
+	<li>WDS Demos GSAP</li>
+	<li>Netflix clone (WDS Demos)</li>
+</ul>
+
+<h2>Pet Project Websites</h2>
+
+<ul>
+	<li>Time Tracker</li>
+	<li>Holiday Dashboard</li>
+	<li>MKRecords (designs and site)</li>
+	<li>NonZZMT Site</li>
+</ul>
+
+<h2>3D/Design Experiments</h2>
+<ul>
+	<li>r3f flex-menu codesandbox</li>
+	<li>r3f obscuring label</li>
+	<li>r3f obscuring label</li>
+	<li>generative art (from frontendmasters course)</li>
+	<li>new tab colour-changing selector thing</li>
+	<li>logo animations</li>
+</ul>
+
+<!-- <ul>
 	<li>wds demos gsap</li>
 	<li>noseynewtab</li>
 	<li>monumental</li>
@@ -17,4 +43,4 @@
 	<li>r3f exploding cube codesandbox</li>
 	<li>generative art frontendmasters things</li>
 	<li>mkrecords designs and site</li>
-</ul>
+</ul> -->
