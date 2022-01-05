c83a613aa8fb41d8ea8f0c7d9741f24f0c98c8fc
Michael Hutchings <mdhutchings94@gmail.com>
2022-01-05 19:40:24 +0000

    add script to download cv page as pdf file

---
+++ scripts/downloadCV.cjs
@@ -0,0 +1,14 @@
+const puppeteer = require('puppeteer')
+ 
+async function printPDF() {
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();
+  await page.goto('http://localhost:4123/cv', {waitUntil: 'networkidle0'});
+  // const cv = await page.evaluate(() => document.querySelector('article').innerHTML)
+  // await page.setContent(cv)
+  const pdf = await page.pdf({ path: 'puppeteerPDF.pdf', format: 'A4' });
+ 
+  await browser.close();
+  return pdf
+}
+printPDF()
